2c15a9df039264c5b66b16975867c6b9678ed469
Alexey Osipenko <aratak@anahoret.com>
2011-07-22 10:41:19 +0300

    socket disconnect has been handeled.

--- chat.js
+++ chat.js
@@ -14,11 +14,19 @@ function onConnection(socket) {
   socket.on('create_message', onMessageCreated);
 }
 
+function onDisconnect(socket) {
+  async.reject(sockets, function (currentSocket) { return currentSocket === socket; }, function(result) {
+    sockets = result;
+  });
+}
 
 exports.init = function(app) {
   var io = socketIo.listen(app);
   io.sockets.on('connection', function(socket) {
     onConnection(socket);
+    socket.on('disconnect', function() {
+      onDisconnect(socket);
+    });
   });
 }
 
